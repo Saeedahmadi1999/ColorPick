836cb0224e0d946ce369be785232008304fa2078
saeed <saeedgit.angular@gmail.com>
2019-06-29 15:14:27 +0300

    add my scripts

--- colorgame.js
+++ colorgame.js
@@ -1,95 +1,91 @@
-        //define the varyable
-var colors = connect(6);
-var squares = document.querySelectorAll(".squer");
-var pickedcolor = colors[h1Change()];
-var colorDisplay = document.getElementById("h1");
-var span = document.querySelector("#placeholder");
-var header = document.getElementById("header");
-var easy = document.getElementById("easy");
-var hard = document.getElementById("hard");
-var resetBtn = document.querySelector("#reset");
-var easyBtn = document.querySelector(".easy");
-var hardBtn = document.querySelector(".hard");
+let squars = document.querySelectorAll(".squer");
+let container = document.querySelector("#container");
+let message = document.querySelector("#placeholder");
+let header = document.querySelector("#h1");
+let sqcolor = colorpicker(6);
+let pickedColor = sqcolor[Math.floor(Math.random() * 6)];
+let resetbtn = document.querySelector('#reset');
+let hard = document.querySelector('.hard');
+let easy = document.querySelector('.easy');
+let nav = document.querySelector('#header');
+let mode = "";
+let party;
 
+container.addEventListener("click", function(event) {
+    let target = event.target;
 
-        //change the h1 top of page
-colorDisplay.textContent = pickedcolor;
-
-        //easy button
-easyBtn.addEventListener("click", function(){
-   colors = connect(3);
-   for(var i = 0;i < squares.length;i++){
-       if(colors[i]){
-           squares[i].style.background = colors[i]
-       }else{
-           squares[i].style.display = "none";
-       }
-   }
-    pickedcolor = colors[h1Change()];
-});
-
-        //Hard Button
-hardBtn.addEventListener("click", function() {
-    colors = connect(6);
-    for(var i = 0;i < squares.length;i++) {
-        squares[i].style.display = "block";
+    if(target !== container && mode !== "finish") {
+        if(target.style.background === pickedColor) {
+            message.innerHTML = "You Win";
+            mode = "finish";
+            for (let i = 0; i < squars.length; i++) {
+                squars[i].style.background = pickedColor; 
+            }
+            party = setInterval( () => {
+                sqcolor = colorpicker(6);
+                pickedColor = sqcolor[Math.floor(Math.random() * 6)];
+                for (let i = 0; i < sqcolor.length; i++) {
+                    squars[i].style.background = sqcolor[i]; 
+                }
+                nav.style.background = randomcolor();
+                
+            }, 500)
+        } else {
+            message.innerHTML = "Try Again";
+            target.style.background = "black";
+        }
     }
 });
 
-        //reset button
-resetBtn.addEventListener("click", function(){
-    
-    pickedcolor = colors[h1Change()];
-    for(var i = 0;i < squares.length;i++){
-        squares[i].style.background = colors[i];
-    }
-    colorDisplay.textContent = pickedcolor;
-    resetBtn.textContent = "NewGame";
-    span.textContent = "";
-    header.style.background = "dodgerblue";
+resetbtn.addEventListener('click', function() {
+    reset(6);
 });
-        //paint squares
-for(var i = 0;i < squares.length;i++){
-    squares[i].style.background = colors[i];
-    squares[i].addEventListener("click", function(){
-       var clickedColor = this.style.background;
-        //compare h1 and clickedcolor
-       if(clickedColor === pickedcolor){
-           span.textContent = "correct!!";
-           headshot()
-           resetBtn.textContent = "playAgain?";
-       } else{
-           this.style.background = "black";
-           span.textContent = "try again!!";
-       }
-    });
-}
-        //function for correct guess
-function headshot(){
-    for(var i = 0;i< squares.length;i++){
-        squares[i].style.background = pickedcolor;
-        header.style.background = pickedcolor; 
-        
-    }
-}
-        //function for random h1
-function h1Change(){
-  var num = Math.floor(Math.random()*colors.length);
-  return(num);  
-}
-        //function for colors
-function connect(num){
-    var arr = [];
-    for(var i = 0;i < num;i++){
-        arr.push(detailConnect());
-    }
-    return(arr);
-}
-function detailConnect(){
-    var r = Math.floor(Math.random()*256);
-    var g = Math.floor(Math.random()*256);
-    var b = Math.floor(Math.random()*256);
-    return("rgb(" + r + ", " +  g + ", " +  b + ")");
-}
-  
 
+
+easy.addEventListener('click', function() {
+    for (let i = 3; i < squars.length; i++) {
+        squars[i].style.background = 'black'; 
+    }
+    reset(3);
+});
+
+
+hard.addEventListener('click', function() {
+    reset(6);
+});
+
+
+{
+    for (let i = 0; i < squars.length; i++) {
+        squars[i].style.background = sqcolor[i]; 
+    }
+    header.innerHTML = pickedColor;
+}
+
+function colorpicker(n) {
+    let colors = [];
+    for (let i = 0; i < n; i++) {
+        colors.push(randomcolor())
+    }
+    return colors;
+}
+function randomcolor() {
+
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+function reset(n) {
+    sqcolor = colorpicker(n);
+    pickedColor = sqcolor[Math.floor(Math.random() * n)];
+    for (let i = 0; i < sqcolor.length; i++) {
+        squars[i].style.background = sqcolor[i]; 
+    }
+    header.innerHTML = pickedColor;
+    mode = '';
+    message.innerHTML = '';
+    clearInterval(party);
+}
